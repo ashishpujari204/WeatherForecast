cbf271313a5dc467bed08fedb8974addab9b8ab3
Ashish Pujari <ashish@amuratech.com>
2015-02-18 17:31:35 +0530

    Checking Branching...test

--- src/com/app/weatherforecast/ByGps.java
+++ src/com/app/weatherforecast/ByGps.java
@@ -41,7 +41,7 @@ public class ByGps extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_by_gps);
 
-		init();//by gps...Branching...:)
+		init();//by gps...Branching...:).......
 	}
 
 	private void init() {
